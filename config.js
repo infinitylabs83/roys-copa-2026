82a192660bf6f202ed14b89e3da2744169257397
Gustavo <gustavohgo@hotmail.com>
2026-06-14 23:15:36 -0300

    Configure production deployment and protect private files

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.ROYS_CONFIG = window.ROYS_CONFIG || {
-  SUPABASE_URL: "",
-  SUPABASE_PUBLISHABLE_KEY: "",
+  SUPABASE_URL: "https://igahedafojvdcvukcbco.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_FzdvUqvCHdu6MyoMxerZiA_QxtwzALO",
   CAMPAIGN_SLUG: "copa-roys-2026",
   START_GAME_FUNCTION: "start-game",
   FINISH_GAME_FUNCTION: "finish-game",
-  PUBLIC_URL: "https://copa.roys.com.br",
+  PUBLIC_URL: "https://roys-copa-2026.vercel.app",
   LOCAL_DEMO_CODES: ["ROY123", "COP202", "GOL777"]
 };
